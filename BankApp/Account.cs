feec6ba4627a580f3e74fc010994b917bf4b3b93
garytriv <59942593+garytriv@users.noreply.github.com>
2020-01-24 08:44:49 -0800

    initial 1st commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    class Account
+    {
+        public int AccountNumber { get; set; }
+
+        public string AccountName { get; set; }
+        public decimal Balance { get; set; }
+
+        public string AccountType { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
